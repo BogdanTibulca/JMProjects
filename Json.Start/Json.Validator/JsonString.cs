629776570b53a6cd2c2901a8521df4e1e7263780
BogdanTibulca <bogdantibulca@yahoo.com>
2021-01-08 12:31:32 +0200

    Updated IsJsonString function to check whether the input is null or empty. Created IsWrappedInQuotes function and refactored IsJsonString function.

--- Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Validator/JsonString.cs
@@ -6,6 +6,11 @@ namespace Json
     {
         public static bool IsJsonString(string input)
         {
+            return !string.IsNullOrEmpty(input) && IsWrappedInQuotes(input);
+        }
+
+        private static bool IsWrappedInQuotes(string input)
+        {
             const char Quotes = '\"';
 
             return input[0] == Quotes && input[^1] == Quotes;
