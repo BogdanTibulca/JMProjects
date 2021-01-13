9699595a0e15c64860dd3e7e3cff50fff32ba611
BogdanTibulca <bogdantibulca@yahoo.com>
2021-01-13 15:04:39 +0200

    Updated IsValidEscapedCharacter to accept escaped horizontal tabs.

--- Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Validator/JsonString.cs
@@ -36,7 +36,7 @@ namespace Json
 
         private static bool IsValidEscapedCharacter(char escaped)
         {
-            char[] validChars = { '"', '\\', '/', 'b', 'f', 'n', 'r' };
+            char[] validChars = { '"', '\\', '/', 'b', 'f', 'n', 'r', 't' };
 
             foreach (char ch in validChars)
             {
