e4b8604fb4c2470a78a9e2e947c6baad969a7301
BogdanTibulca <bogdantibulca@yahoo.com>
2021-01-13 14:57:25 +0200

    Updated IsValidEscapedCharacter to accept escaped form feed.

--- Json.Start/Json.Validator/JsonString.cs
+++ Json.Start/Json.Validator/JsonString.cs
@@ -36,7 +36,7 @@ namespace Json
 
         private static bool IsValidEscapedCharacter(char escaped)
         {
-            char[] validChars = { '"', '\\', '/', 'b' };
+            char[] validChars = { '"', '\\', '/', 'b', 'f' };
 
             foreach (char ch in validChars)
             {
