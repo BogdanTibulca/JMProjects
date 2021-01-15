8b06670d6761e882dcea7dd1a0ec4f30aaccae52
BogdanTibulca <bogdantibulca@yahoo.com>
2021-01-15 13:46:34 +0200

    Added ContainsLetters function and updated IsJsonNumber function.

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -6,7 +6,21 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            return char.IsDigit(input[0]);
+            return char.IsDigit(input[0]) &&
+                !ContainsLetters(input);
+        }
+
+        private static bool ContainsLetters(string input)
+        {
+            foreach (char ch in input)
+            {
+                if (char.IsLetter(ch))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
