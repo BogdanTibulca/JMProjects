5f305ca52b050b270ec15d6ac5bc5c598b364433
BogdanTibulca <bogdantibulca@yahoo.com>
2021-01-15 14:20:41 +0200

    IsNull function created and updated IsJsonNumber.

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -6,7 +6,13 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            return ContainsOnlyDiggits(input);
+            return !IsNull(input) &&
+                    ContainsOnlyDiggits(input);
+        }
+
+        private static bool IsNull(string input)
+        {
+            return string.IsNullOrEmpty(input);
         }
 
         private static bool ContainsOnlyDiggits(string input)
