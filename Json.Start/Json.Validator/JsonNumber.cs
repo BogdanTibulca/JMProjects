eb7a3c6d941ba6361183811f3cac95ce0e742769
BogdanTibulca <bogdantibulca@yahoo.com>
2021-01-18 13:24:43 +0200

    IsValidFractionalNumber created.

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -4,10 +4,12 @@ namespace Json
 {
     public static class JsonNumber
     {
+        const char DecimalSeparator = '.';
+
         public static bool IsJsonNumber(string input)
         {
             return !string.IsNullOrEmpty(input) &&
-                    IsValidInteger(input);
+                    (IsValidInteger(input) || IsValidFractionalNumber(input));
         }
 
         private static bool IsValidInteger(string input)
@@ -36,6 +38,14 @@ namespace Json
         private static bool IsValidNegativeInteger(string input)
         {
             return input[0] == '-' && IsValidPositiveInteger(input.Substring(1));
+        }
+
+        private static bool IsValidFractionalNumber(string input)
+        {
+            string[] numbersParts = input.Split(DecimalSeparator);
+
+            return IsValidInteger(numbersParts[0]) &&
+                IsValidPositiveInteger(numbersParts[1]);
         }
     }
 }
