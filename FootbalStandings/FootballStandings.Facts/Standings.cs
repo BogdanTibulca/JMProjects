cd3fc3929791f9bbb11e9d2da1e9730483eed47c
BogdanTibulca <bogdantibulca@yahoo.com>
2021-01-27 21:01:54 +0200

    Added the Standings class with methods to retrieve a team by name/rank. AddTeamsInStandings added to add new teams and update the standings.

---
+++ FootbalStandings/FootballStandings.Facts/Standings.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace FootballStandings.Facts
+{
+    class Standings
+    {
+        Team[] teams;
+
+        public Standings(Team[] teams)
+        {
+            this.teams = teams;
+            UpdateStandings();
+        }
+
+        public Team GetTeamByRanking(int rank)
+        {
+            return (rank < 1 || rank >= this.teams.Length) ? null : teams[rank - 1];
+        }
+
+        public string GetTeamRanking(Team teamToFind)
+        {
+            for (int i = 0; i < teams.Length; i++)
+            {
+                if (teams[i].HasTheSameName(teamToFind))
+                {
+                    return (i + 1).ToString();
+                }
+            }
+
+            return string.Format("The team {0} is not in the standings", teamToFind);
+        }
+
+        public Team[] AddTeamInStandings(Team[] teamsToAdd)
+        {
+            if (teamsToAdd.Length == 0)
+            {
+                return teams;
+            }
+
+            int lastElement = this.teams.Length;
+
+            Array.Resize(ref this.teams, this.teams.Length + teamsToAdd.Length);
+            Array.Copy(teamsToAdd, 0, this.teams, lastElement, teamsToAdd.Length);
+
+            UpdateStandings();
+
+            return teams;
+        }
+
+        public void SortTeamsByPoints(Team[] teams)
+        {
+            for (int i = 1; i < teams.Length; ++i)
+            {
+                Team key = teams[i];
+                int j = i - 1;
+
+                while (j >= 0 && key.HasMorePointsThan(teams[j]))
+                {
+                    teams[j + 1] = teams[j];
+                    j--;
+                }
+
+                teams[j + 1] = key;
+            }
+        }
+
+        public void UpdateStandings()
+        {
+            SortTeamsByPoints(this.teams);
+        }
+    }
+}
