1dffa8a3af53989b69ff92285d13d6950c11640b
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-01 19:54:08 +0200

    Created new project. Added the rules for code validation.

---
+++ JSONFormat/JSONFormat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSONFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
