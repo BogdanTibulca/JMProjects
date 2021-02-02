0afa4c7e3432a1011358cd8d361771e0e312e919
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-02 21:18:25 +0200

    Added the test to check whether the match method works correctly with more complex patterns.

--- JSONFormat/JSONFormat.Tests/ChoiceTests.cs
+++ JSONFormat/JSONFormat.Tests/ChoiceTests.cs
@@ -23,5 +23,32 @@ namespace JSONFormat.Tests
 
             Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData("012", true)]
+        [InlineData("12", true)]
+        [InlineData("92", true)]
+        [InlineData("a2", true)]
+        [InlineData("f8", true)]
+        [InlineData("A9", true)]
+        [InlineData("F8", true)]
+        [InlineData("g8", false)]
+        [InlineData("G8", false)]
+        [InlineData("", false)]
+        [InlineData(null, false)]
+
+        public void Match_ComplexPatternsOfCharactersAndRanges_ShouldValidateText(string text, bool expected)
+        {
+            Choice hex = new Choice(
+                diggit,
+                new Choice(
+                    new Range('a', 'f'),
+                    new Range('A', 'F'))
+                );
+
+            bool result = hex.Match(text);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
