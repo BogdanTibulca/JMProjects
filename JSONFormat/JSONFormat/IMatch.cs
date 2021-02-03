e3421b48af9107345e2ea3c302258bdeba6085cb
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-03 18:19:52 +0200

    Created the interface IMatch.

---
+++ JSONFormat/JSONFormat/IMatch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONFormat
+{
+    interface IMatch
+    {
+        bool Success();
+
+        string RemainingText();
+    }
+}
