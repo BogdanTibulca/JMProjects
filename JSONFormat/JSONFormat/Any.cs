dc3524ad6e30912234c2e2faa7aef4302c178285
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-10 18:42:03 +0200

    Created the Any class.

---
+++ JSONFormat/JSONFormat/Any.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONFormat
+{
+    public class Any : IPattern
+    {
+        private readonly string accepted;
+
+        public Any(string accepted)
+        {
+            this.accepted = accepted;
+        }
+
+        public IMatch Match(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
