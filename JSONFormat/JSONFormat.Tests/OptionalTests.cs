e482508ca7f4ea527adf63f67e8ae6a2c6202404
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-10 20:10:08 +0200

    Added the tests for the Optional class.

---
+++ JSONFormat/JSONFormat.Tests/OptionalTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+namespace JSONFormat.Tests
+{
+    public class OptionalTests
+    {
+        private readonly Optional a = new Optional(new Character('a'));
+
+        [Theory]
+        [InlineData("", true, "")]
+        [InlineData(null, true, null)]
+        [InlineData("abc" , true, "bc")]
+        [InlineData("aabc", true, "abc")]
+        [InlineData("bc", true, "bc")]
+        public void Match_AppliesCharacterPattern_ShouldReturnSuccessAndTheRemainingText(
+            string text, bool expectedSuccess, string expectedRemainingText)
+        {
+            IMatch match = a.Match(text);
+
+            Assert.Equal(expectedRemainingText, match.RemainingText());
+            Assert.True(expectedSuccess);
+        }
+
+        private readonly Optional sign = new Optional(new Character('-'));
+        
+        [Theory]
+        [InlineData("-123", true, "123")]
+        [InlineData("123", true, "123")]
+        public void Match_SignCharacterPattern_ShouldReturnSuccessAndTheRemainingText(
+            string text, bool expectedSuccess, string expectedRemainingText)
+        {
+            IMatch match = sign.Match(text);
+
+            Assert.Equal(expectedRemainingText, match.RemainingText());
+            Assert.True(expectedSuccess);
+        }
+    }
+}
