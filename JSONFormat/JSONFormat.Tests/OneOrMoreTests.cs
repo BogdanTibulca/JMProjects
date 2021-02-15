bf0250dbac353141d49e994d178a2cb527eb9654
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-15 19:06:05 +0200

    Added the tests for OneOrMore class.

---
+++ JSONFormat/JSONFormat.Tests/OneOrMoreTests.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace JSONFormat.Tests
+{
+    public class OneOrMoreTests
+    {
+        private readonly OneOrMore a = new OneOrMore(new Range('0', '9'));
+
+        [Theory]
+        [InlineData("", false, "")]
+        [InlineData(null, false, null)]
+        [InlineData("123", true, "")]
+        [InlineData("1a", true, "a")]
+        [InlineData("bc", false, "bc")]
+        public void Match_PatternReceived_ReturnsCorrectSuccessAndRemainingTextAfterRemovingFirstOccurenciesOfThePattern(
+            string text, bool expectedSuccess, string expectedRemainingText)
+        {
+            IMatch match = a.Match(text);
+
+            Assert.Equal(expectedSuccess, match.Success());
+            Assert.Equal(expectedRemainingText, match.RemainingText());
+        }
+    }
+}
