80b0c2d0ccb2cdfc3f4fa8812d799ae3b97e32f0
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-15 19:08:24 +0200

    Created the OneOrMore class.

---
+++ JSONFormat/JSONFormat/OneOrMore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONFormat
+{
+    public class OneOrMore : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = new Many(pattern);
+        }
+
+        public IMatch Match(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
