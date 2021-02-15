debd4337780a4278189bc4be8c47143d07548570
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-15 18:08:48 +0200

    Added another test to ChoiceTests to correctly validate complex patterns.

--- JSONFormat/JSONFormat.Tests/ChoiceTests.cs
+++ JSONFormat/JSONFormat.Tests/ChoiceTests.cs
@@ -50,5 +50,28 @@ namespace JSONFormat.Tests
 
             Assert.Equal(expected, result);
         }
+
+        Choice seq = new Choice(
+            new Character('0'),
+            new Sequence(
+                new Range('a', 'f'),
+                new Range('A', 'F'))
+            );
+
+        [Theory]
+        [InlineData("", false, "")]
+        [InlineData(null, false, null)]
+        [InlineData("0123", true, "123")]
+        [InlineData("a", false, "a")]
+        [InlineData("abc", false, "abc")]
+        [InlineData("aAf", true, "f")]
+        [InlineData("Aaf", false, "Aaf")]
+        public void Match_ComplexPatternReceived_ShouldReturnCorrectSuccessAndRemainingTextResults(string text, bool expectedSuccess, string expectedRemainingText)
+        {
+            IMatch match = seq.Match(text);
+
+            Assert.Equal(expectedSuccess, match.Success());
+            Assert.Equal(expectedRemainingText, match.RemainingText());
+        }
     }
 }
