ba37f2b58c9bd62875f8d32ccdd15ed9162d022f
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-17 18:11:11 +0200

    Added the class List.

---
+++ JSONFormat/JSONFormat/List.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONFormat
+{
+    public class List : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+
+        }
+
+        public IMatch Match(string text)
+        {
+            return this.pattern.Match(text);
+        }
+    }
+}
