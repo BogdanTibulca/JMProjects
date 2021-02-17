9499321f09fd4e7c8607f242180b1a19d2b7bccc
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-17 18:08:49 +0200

    Added the tests for the class List.

---
+++ JSONFormat/JSONFormat.Tests/ListTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+
+namespace JSONFormat.Tests
+{
+    public class ListTests
+    {
+        private readonly List a = new List(
+            new Range('0', '9'),
+            new Character(',')
+            );
+
+        [Theory]
+        [InlineData("", true, "")]
+        [InlineData(null, true, null)]
+        [InlineData("1,2,3", true, "")]
+        [InlineData("1,2,3,", true, ",")]
+        [InlineData("1a", true, "a")]
+        [InlineData("abc", true, "abc")]
+        public void Match_SimpleElementAndSeparatorReceived_ShouldReturnSuccessAndTheRemaininTextAfterApplyingThePattern(
+            string text, bool expectedSuccess, string expectedRemainingText)
+        {
+            IMatch match = a.Match(text);
+
+            Assert.Equal(expectedSuccess, match.Success());
+            Assert.Equal(expectedRemainingText, match.RemainingText());
+        }
+
+        [Theory]
+        [InlineData("1; 22  ;\n 333 \t; 22", true, "")]
+        [InlineData("1 \n;", true, " \n;")]
+        [InlineData("abc", true, "abc")]
+        public void Match_ComplexSeparatorReceived_ShouldReturnSuccessAndTheRemaininTextAfterApplyingThePattern(
+            string text, bool expectedSuccess, string expectedRemainingText)
+        {
+            OneOrMore digits = new OneOrMore(new Range('0', '9'));
+            Many whitespace = new Many(new Any(" \r\n\t"));
+            Sequence separator = new Sequence(whitespace, new Character(';'), whitespace);
+            List list = new List(digits, separator);
+
+            IMatch match = list.Match(text);
+
+            Assert.Equal(expectedSuccess, match.Success());
+            Assert.Equal(expectedRemainingText, match.RemainingText());
+        }
+    }
+}
