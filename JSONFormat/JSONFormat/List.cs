82e6bc1d5e74dc1abd9feffa1b08e93c179e6a9a
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-18 17:50:16 +0200

    Updated the constructor in the class List.

--- JSONFormat/JSONFormat/List.cs
+++ JSONFormat/JSONFormat/List.cs
@@ -10,7 +10,11 @@ namespace JSONFormat
 
         public List(IPattern element, IPattern separator)
         {
+            Sequence sequence = new Sequence(
+                new OneOrMore(element),
+                new Many(new Sequence(separator, new OneOrMore(element))));
 
+            this.pattern = new Many(sequence);
         }
 
         public IMatch Match(string text)
