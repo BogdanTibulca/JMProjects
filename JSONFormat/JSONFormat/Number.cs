5df1277b40095435b49eeb24b1a76288e4d5d740
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-18 18:40:12 +0200

    Added the class Number.

---
+++ JSONFormat/JSONFormat/Number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONFormat
+{
+    public class Number : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Number()
+        {
+
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
