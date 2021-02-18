cfb687ab90c90a3b0e8cc24522691966e19977ac
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-18 19:19:38 +0200

    Updated the pattern in the class Number to validate numbers with exponents.

--- JSONFormat/JSONFormat/Number.cs
+++ JSONFormat/JSONFormat/Number.cs
@@ -11,12 +11,16 @@ namespace JSONFormat
         public Number()
         {
             IPattern numbersRange = new Range('0', '9');
-            IPattern integerNumber = new OneOrMore(numbersRange);
-            IPattern decimalNumber = new Sequence(
-                integerNumber,
-                new Optional(new Sequence(new Character('.'), integerNumber)));
+            IPattern exponent = new Any("eE");
 
-            this.pattern = new Choice(decimalNumber);
+            IPattern integerNumber = new OneOrMore(numbersRange);
+
+            IPattern exponentialNumber = new Sequence(
+                integerNumber,
+                new Optional(new Sequence(new Character('.'), integerNumber)),
+                new Optional(new Sequence(exponent, integerNumber)));
+
+            this.pattern = new Sequence(exponentialNumber);
         }
 
         public IMatch Match(string text)
