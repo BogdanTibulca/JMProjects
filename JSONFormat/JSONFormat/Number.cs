0b2a96150d16bc4bd36daec12e40449ed242079e
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-18 18:43:34 +0200

    Updated the Number class to validate an integer.

--- JSONFormat/JSONFormat/Number.cs
+++ JSONFormat/JSONFormat/Number.cs
@@ -7,10 +7,11 @@ namespace JSONFormat
     public class Number : IPattern
     {
         private readonly IPattern pattern;
+        private readonly IPattern numbersRange = new Range('0', '9');
 
         public Number()
         {
-
+            this.pattern = new OneOrMore(numbersRange);
         }
 
         public IMatch Match(string text)
