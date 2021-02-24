45a92b2cad08ebaeffbf34d05416ddb3534694ab
BogdanTibulca <bogdantibulca@yahoo.com>
2021-02-24 20:15:50 +0200

    Added the String class.

---
+++ JSONFormat/JSONFormat/String.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONFormat
+{
+    public class String : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public String()
+        {
+
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
