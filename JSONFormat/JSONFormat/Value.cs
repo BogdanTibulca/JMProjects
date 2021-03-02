f7e604db8820ba86b38dd9060c12c4177e35ae58
BogdanTibulca <bogdantibulca@yahoo.com>
2021-03-02 18:36:25 +0200

    Created the class Value.

---
+++ JSONFormat/JSONFormat/Value.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONFormat
+{
+    public class Value : IPattern
+    {
+        private readonly IPattern pattern;
+
+        public Value()
+        {
+
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
