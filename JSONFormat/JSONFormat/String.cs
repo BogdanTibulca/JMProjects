36e689b4261229c55a6e6f6a567f260f8281c0fc
BogdanTibulca <bogdantibulca@yahoo.com>
2021-03-02 17:52:39 +0200

    Updated the pattern in the class String.

--- JSONFormat/JSONFormat/String.cs
+++ JSONFormat/JSONFormat/String.cs
@@ -11,20 +11,13 @@ namespace JSONFormat
         public String()
         {
             var quotes = new Character('"');
-            var digit = new Range('0', '9');
-            var hexaLetters = new Choice(new Range('A', 'F'), new Range('a', 'f'));
-
-            var hex = new Sequence(
-                new Choice(digit, hexaLetters),
-                new Choice(digit, hexaLetters),
-                new Choice(digit, hexaLetters),
-                new Choice(digit, hexaLetters));
+            var hex = new Choice(new Range('A', 'F'), new Range('a', 'f'), new Range('0', '9'));
 
             var escaped = new Sequence(
                 new Character('\\'),
                 new Choice(
                     new Any("\"\\/bfnrt"),
-                    new Sequence(new Character('u'), hex)));
+                    new Sequence(new Character('u'), hex, hex, hex, hex)));
 
             var character = new Choice(
                 new Character('\u0020'),
