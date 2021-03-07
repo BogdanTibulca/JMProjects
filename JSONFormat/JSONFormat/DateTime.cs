24f30e6d8baab09eb9f1316c2eb0f91fe676b9b6
BogdanTibulca <bogdantibulca@yahoo.com>
2021-03-07 19:52:46 +0200

    Updated the format of the pattern for the DateTime class.

--- JSONFormat/JSONFormat/DateTime.cs
+++ JSONFormat/JSONFormat/DateTime.cs
@@ -6,12 +6,42 @@ namespace JSONFormat
 {
     public class DateTime : IPattern
     {
-
         private readonly IPattern pattern;
 
         public DateTime()
         {
-           
+            var dateSeparator = new Character('-');
+            var timeSeparator = new Character(':');
+            var digit = new Range('0', '9');
+
+            var year = new Sequence(digit, digit, digit, digit);
+            var twoDigits = new Sequence(digit, digit);
+
+            var date = new Sequence(
+                year,
+                dateSeparator,
+                twoDigits,
+                dateSeparator,
+                twoDigits);
+
+            var time = new Sequence(
+                new Character('T'),
+                twoDigits,
+                timeSeparator,
+                twoDigits,
+                timeSeparator,
+                twoDigits,
+                new Optional(new Sequence(new Character('.'), new OneOrMore(digit))));
+
+            var timeZoneDesignator = new Choice(
+                new Character('Z'),
+                new Sequence(
+                    new Choice(new Character('+'), new Character('-')),
+                    twoDigits,
+                    timeSeparator,
+                    twoDigits));
+
+            this.pattern = new Sequence(date, time, timeZoneDesignator);
         }
 
         public IMatch Match(string text)
