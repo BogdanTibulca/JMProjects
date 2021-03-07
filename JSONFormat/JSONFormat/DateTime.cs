8c6298bd93942f4f39ba2fc88884b51d5904a5dd
BogdanTibulca <bogdantibulca@yahoo.com>
2021-03-07 18:58:23 +0200

    Created the DateTime class.

---
+++ JSONFormat/JSONFormat/DateTime.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONFormat
+{
+    public class DateTime : IPattern
+    {
+
+        private readonly IPattern pattern;
+
+        public DateTime()
+        {
+           
+        }
+
+        public IMatch Match(string text)
+        {
+            return this.pattern.Match(text);
+        }
+    }
+}
