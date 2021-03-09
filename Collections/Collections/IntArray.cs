6a09ca1f13fc6727c3a879eb8b31434add4189c2
BogdanTibulca <bogdantibulca@yahoo.com>
2021-03-09 17:57:03 +0200

    Added the class IntArray. Implemented the methods Add and Element.

---
+++ Collections/Collections/IntArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Collections
+{
+    public class IntArray
+    {
+        private int[] array;
+
+        public IntArray()
+        {
+            this.array = new int[0];
+        }
+
+        public void Add(int element)
+        {
+            Array.Resize(ref this.array, this.array.Length + 1);
+
+            this.array[this.array.Length - 1] = element;
+        }
+
+        public int? Element(int index)
+        {
+            if (index < 0 || index > this.array.Length - 1)
+            {
+                return null;
+            }
+
+            return this.array[index];
+        }
+    }
+}
