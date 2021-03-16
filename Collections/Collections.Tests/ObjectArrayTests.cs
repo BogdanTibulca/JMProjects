5953e65c3c4da6aa7f37e81af742679d8d748d25
BogdanTibulca <bogdantibulca@yahoo.com>
2021-03-16 19:20:01 +0200

    Added tests for the ObjectArray class.

---
+++ Collections/Collections.Tests/ObjectArrayTests.cs
@@ -0,0 +1,146 @@
+﻿using Xunit;
+
+namespace Collections.Tests
+{
+    public class ObjectArrayTests
+    {
+        private ObjectArray objArr = new ObjectArray();
+
+        [Fact]
+        public void Add_AddsAnObjectAtTheEndOfTheArray()
+        {
+            objArr.Add(1);
+            objArr.Add("a string");
+            objArr.Add(true);
+            objArr.Add('c');
+
+            Assert.Equal(1, objArr[0]);
+            Assert.True(objArr[1].Equals("a string"));
+            Assert.True(objArr[2]);
+            Assert.Equal('c', objArr[3]);
+        }
+
+        [Fact]
+        public void Count_ReturnsTheNumberOfObjectsInTheArray()
+        {
+            objArr.Add(1);
+            objArr.Add("a string");
+            objArr.Add(true);
+            objArr.Add('c');
+
+            Assert.Equal(4, objArr.Count);
+        }
+
+        [Fact]
+        public void SetElement_ChangesTheValueOfAnObjectAtAGivenIndex()
+        {
+            objArr.Add(1);
+            objArr.Add("a string");
+            objArr.Add(true);
+
+            objArr[1] = "new string";
+            objArr[2] = false;
+
+            Assert.True(objArr[1].Equals("new string"));
+            Assert.False(objArr[2]);
+        }
+
+        [Theory]
+        [InlineData(123, true)]
+        [InlineData(12, false)]
+        [InlineData("a string", true)]
+        [InlineData("not here", false)]
+        [InlineData(true, true)]
+        [InlineData(false, false)]
+        [InlineData('c', true)]
+        [InlineData('+', false)]
+        public void Contains_ChecksWhetherTheObjectIsInTheArray(
+            object element, bool expectedResult)
+        {
+            objArr.Add(123);
+            objArr.Add("a string");
+            objArr.Add(true);
+            objArr.Add('c');
+
+            Assert.Equal(expectedResult, objArr.Contains(element));
+        }
+
+        [Theory]
+        [InlineData(123, 0)]
+        [InlineData(12, -1)]
+        [InlineData("a string", 1)]
+        [InlineData("string", -1)]
+        [InlineData(true, 2)]
+        [InlineData(false, -1)]
+        [InlineData('c', 3)]
+        [InlineData('-', -1)]
+        public void IndexOf_ReturnsTheIndexOfAGivenObject(
+            object element, int expectedIndex)
+        {
+            objArr.Add(123);
+            objArr.Add("a string");
+            objArr.Add(true);
+            objArr.Add('c');
+
+            Assert.Equal(expectedIndex, objArr.IndexOf(element));
+        }
+
+        [Fact]
+        public void Insert_ShouldInsertANewObjectAtAGivenIndex()
+        {
+            objArr.Add('a');
+            objArr.Add(22);
+            objArr.Add(false);
+
+            objArr.Insert(1, "March");
+
+            Assert.Equal('a', objArr[0]);
+            Assert.Equal(22, objArr[1]);
+            Assert.False(objArr[2]);
+            Assert.True(objArr[3].Equals("March"));
+        }
+
+        [Fact]
+        public void Clear_RemovesAllTheObjectsInTheArray()
+        {
+            objArr.Add(123);
+            objArr.Add("a string");
+            objArr.Add(true);
+            objArr.Add('c');
+
+            objArr.Clear();
+
+            Assert.Equal(0, objArr.Count);
+        }
+
+        [Fact]
+        public void Remove_RemovesTheFirstApperanceOfAGivenObject()
+        {
+            objArr.Add(123);
+            objArr.Add("a string");
+            objArr.Add('c');
+            objArr.Add(false);
+
+            objArr.Remove('c');
+
+            Assert.Equal(123, objArr[0]);
+            Assert.True(objArr[1].Equals("a string"));
+            Assert.False(objArr[2]);
+        }
+
+        [Fact]
+        public void RemoveAt_RemovesTheObjectAtTheGivenPosition()
+        {
+            objArr.Add(123);
+            objArr.Add("a string");
+            objArr.Add('c');
+            objArr.Add(false);
+
+            objArr.RemoveAt(2);
+
+            Assert.Equal(123, objArr[0]);
+            Assert.True(objArr[1].Equals("a string"));
+            Assert.False(objArr[2]);
+        }
+    }
+}
